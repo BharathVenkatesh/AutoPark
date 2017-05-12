b289d0994182d8404e2b589871347349592f5490
Karl bou khalil <karlboukhalil@172-16-38-28.DYNAPOOL.NYU.EDU>
2017-05-11 21:29:19 -0400

    parallel parking

--- demo6/demo6.c
+++ demo6/demo6.c
@@ -17,41 +17,52 @@
 #include "foo.h"
 
 void parallel_park() {
-    while (encoders_distances.right < 25/*100*/ || encoders_distances.left < 25/*100*/)
-        motors_control(NORMAL, 0.0f, 0.0f, NORMAL);
+    while (encoders_distances.left < 30)
+        motors_control(0.3f, 0.0f, 0.0f, 1.0f);
 
     motors_control(0.0f,0.0f, 0.0f,0.0f);
     cpu_sw_delay(100U);
-    // motors_control(0.0f,0.6f, 0.6f,0.0f);
-    // cpu_sw_delay(50U);
     encoders_distances.right = 0;
     encoders_distances.left = 0;
 
-    while (encoders_distances.right < 11/*100*/ || encoders_distances.left < 11/*100*/)
-        motors_control(0.0f, 1.0f, 1.0f, 0.0f);
+    while (encoders_distances.left < 20)
+        motors_control(0.0f, NORMAL, NORMAL, 0.0f);
 
-    encoders_distances.right = 0;
-    encoders_distances.left = 0;
+    // while (encoders_distances.right < 25/*100*/ || encoders_distances.left < 25/*100*/)
+    //     motors_control(NORMAL, 0.0f, 0.0f, NORMAL);
 
-    while (/*encoders_distances.right < 10100 ||*/ encoders_distances.left < 15/*100*/)
-        motors_control(0.0f, 0.3f, 1.0f, 0.0f);
+    // motors_control(0.0f,0.0f, 0.0f,0.0f);
+    // cpu_sw_delay(100U);
+    // // motors_control(0.0f,0.6f, 0.6f,0.0f);
+    // // cpu_sw_delay(50U);
+    // encoders_distances.right = 0;
+    // encoders_distances.left = 0;
 
-    encoders_distances.right = 0;
-    encoders_distances.left = 0;
+    // while (encoders_distances.right < 11/*100*/ || encoders_distances.left < 11/*100*/)
+    //     motors_control(0.0f, 1.0f, 1.0f, 0.0f);
 
-    while (encoders_distances.right < 6/*100*/ || encoders_distances.left < 6/*100*/)
-        motors_control(0.0f, 1.0f, 1.0f, 0.0f);
+    // encoders_distances.right = 0;
+    // encoders_distances.left = 0;
 
-    encoders_distances.right = 0;
-    encoders_distances.left = 0;
+    // while (/*encoders_distances.right < 10100 ||*/ encoders_distances.left < 15/*100*/)
+    //     motors_control(0.0f, 0.3f, 1.0f, 0.0f);
 
-    while (/*encoders_distances.right < 6100 ||*/ encoders_distances.left < 15/*100*/)
-        motors_control(0.0f, 1.0f, 0.3f, 0.0f);
+    // encoders_distances.right = 0;
+    // encoders_distances.left = 0;
 
-    encoders_distances.right = 0;
-    encoders_distances.left = 0;
+    // while (encoders_distances.right < 6/*100*/ || encoders_distances.left < 6/*100*/)
+    //     motors_control(0.0f, 1.0f, 1.0f, 0.0f);
 
-    //motors_control(0.0f, NORMAL, NORMAL, 0.0f);
+    // encoders_distances.right = 0;
+    // encoders_distances.left = 0;
+
+    // while (/*encoders_distances.right < 6100 ||*/ encoders_distances.left < 15/*100*/)
+    //     motors_control(0.0f, 1.0f, 0.3f, 0.0f);
+
+    // encoders_distances.right = 0;
+    // encoders_distances.left = 0;
+
+    // //motors_control(0.0f, NORMAL, NORMAL, 0.0f);
 }
 
 void perpendicular_park() {
@@ -274,6 +285,8 @@ int main()
                             encoders_distances.right = 0;
                             encoders_distances.left = 0;
                             perpendicular_park();
+                            //par_park = 1;
+                            //parallel_park();
                            // break;
                             count = FRONTQUEUEMAX + 1;
                             motorState = BACK;
@@ -472,7 +485,10 @@ int main()
             else if (motorState == BACK) {
                 motors_control(0.0f, NORMAL, NORMAL, 0.0f);
 
-                if (distances.right <= 15.0f) {
+                if (distances.right <= 25.4f) {
+                    motors_control(0.0f, 0.0f, 0.0f, 0.0f);
+                    cpu_sw_delay(100U);
+                    parallel_park();
                     motorState = STOP;
                 }
             }
