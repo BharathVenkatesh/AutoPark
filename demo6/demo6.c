05cbec930f890c42ebe75ae1228577aac2fcc401
Karl bou khalil <karlboukhalil@172-16-38-28.DYNAPOOL.NYU.EDU>
2017-05-11 20:24:34 -0400

    update

--- demo6/demo6.c
+++ demo6/demo6.c
@@ -17,13 +17,7 @@
 #include "foo.h"
 
 void parallel_park() {
-    while (encoders_distances.right < 20/*100*/ && encoders_distances.left < 20/*100*/)
-        motors_control(NORMAL, 0.0f, 0.0f, NORMAL);
-    motors_control(0.0f,0.0f,0.0f,0.0f);
-}
-
-void perpendicular_park() {
-    while (/*encoders_distances.right < 10/*100 &&*/ encoders_distances.left < 30/*100*/)
+    while (encoders_distances.right < 25/*100*/ || encoders_distances.left < 25/*100*/)
         motors_control(NORMAL, 0.0f, 0.0f, NORMAL);
 
     motors_control(0.0f,0.0f, 0.0f,0.0f);
@@ -33,16 +27,61 @@ void perpendicular_park() {
     encoders_distances.right = 0;
     encoders_distances.left = 0;
 
-    while (/*encoders_distances.right < 30100 &&*/ encoders_distances.left < 10/*100*/)
+    while (encoders_distances.right < 11/*100*/ || encoders_distances.left < 11/*100*/)
         motors_control(0.0f, 1.0f, 1.0f, 0.0f);
 
     encoders_distances.right = 0;
     encoders_distances.left = 0;
 
-    while (/*encoders_distances.right < 30100 &&*/ encoders_distances.left < 30/*100*/)
+    while (/*encoders_distances.right < 10100 ||*/ encoders_distances.left < 15/*100*/)
         motors_control(0.0f, 0.3f, 1.0f, 0.0f);
 
-    motors_control(0.0f,0.0f,0.0f,0.0f);
+    encoders_distances.right = 0;
+    encoders_distances.left = 0;
+
+    while (encoders_distances.right < 6/*100*/ || encoders_distances.left < 6/*100*/)
+        motors_control(0.0f, 1.0f, 1.0f, 0.0f);
+
+    encoders_distances.right = 0;
+    encoders_distances.left = 0;
+
+    while (/*encoders_distances.right < 6100 ||*/ encoders_distances.left < 15/*100*/)
+        motors_control(0.0f, 1.0f, 0.3f, 0.0f);
+
+    encoders_distances.right = 0;
+    encoders_distances.left = 0;
+
+    //motors_control(0.0f, NORMAL, NORMAL, 0.0f);
+}
+
+void perpendicular_park() {
+    while (encoders_distances.right < 25/*100*/ || encoders_distances.left < 25/*100*/)
+        motors_control(NORMAL, 0.0f, 0.0f, NORMAL);
+
+    motors_control(0.0f,0.0f, 0.0f,0.0f);
+    cpu_sw_delay(100U);
+    // motors_control(0.0f,0.6f, 0.6f,0.0f);
+    // cpu_sw_delay(50U);
+    encoders_distances.right = 0;
+    encoders_distances.left = 0;
+
+    while (encoders_distances.right < 11/*100*/ || encoders_distances.left < 11/*100*/)
+        motors_control(0.0f, 1.0f, 1.0f, 0.0f);
+
+    encoders_distances.right = 0;
+    encoders_distances.left = 0;
+
+    while (/*encoders_distances.right < 10100 ||*/ encoders_distances.left < 30/*100*/)
+        motors_control(0.0f, 0.3f, 1.0f, 0.0f);
+
+    motors_control(0.0f, NORMAL, NORMAL, 0.0f);
+
+    encoders_distances.right = 0;
+    encoders_distances.left = 0;
+
+    distances.right = 30.0f;
+
+    //motors_control(0.0f,0.0f,0.0f,0.0f);
 }
 
 void init_sensors_values() {
@@ -126,6 +165,7 @@ int main()
     int countRight = 0;
     int turned_left = 0;
     delay = 0;
+    found = 0;
     // float wall = 0.0f;
 
     /* Initialize leds for debugging */
@@ -227,13 +267,17 @@ int main()
                 // //         motorState = STOP;
                 // // }
                 else {
-                    if (encoders_distances.left >= /*350*/20 && encoders_distances.right >= /*350*/20 && distances.right < 25.0f) {
-                        encoders_distances.right = 0;
-                        encoders_distances.left = 0;
-                        perpendicular_park();
-                       // break;
-                        count = FRONTQUEUEMAX + 1;
-                        motorState = STOP;
+                    if (encoders_distances.left >= /*350*/20 && encoders_distances.right >= /*350*/20 /*&& distances.right < 25.0f*/) {
+                        if (found == 0 && distances.right > 25.0f)
+                            found = 1;
+                        else if (found == 1 && distances.right < 25.0f) {
+                            encoders_distances.right = 0;
+                            encoders_distances.left = 0;
+                            perpendicular_park();
+                           // break;
+                            count = FRONTQUEUEMAX + 1;
+                            motorState = BACK;
+                        }
                     }
                 }
                 
@@ -424,6 +468,13 @@ int main()
                 // set_pwm(right_pwmPD7, 0.0f);
                 // set_pwm(left_pwmPD3, 0.0f);
                 // set_pwm(left_pwmPD4, 0.8f);
+            }
+            else if (motorState == BACK) {
+                motors_control(0.0f, NORMAL, NORMAL, 0.0f);
+
+                if (distances.right <= 15.0f) {
+                    motorState = STOP;
+                }
             }
 
             /* Trigger sensors */
