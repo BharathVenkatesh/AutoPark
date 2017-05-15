032502a20ab2897964f3f99e815d5eb408bee24e
ditihala <ditihala@gmail.com>
2017-05-14 20:18:15 -0400

    added comments on hal_sensors.c

--- demo6/user_source/hal_interface/encoder_hal.c
+++ demo6/user_source/hal_interface/encoder_hal.c
@@ -1,8 +1,8 @@
 #include "encoder_hal.h"
 #include "cpu.h"
 int count = 0;
-int lock_right;
-int lock_left;
+int lock_right; //Semaphore for the right encoder
+int lock_left;  //Semaphore for the left encoder
 
 void init_encoders(void) {
 	static GPIO_InitTypeDef  Encoders;
@@ -33,49 +33,50 @@ void EXTI3_IRQHandler(void) {
 	HAL_GPIO_EXTI_IRQHandler(encodersPins.right);
 }
 
+//Encoder Interrupt Callback
 void encoders_Callback(uint16_t GPIO_Pin) {
 	if (GPIO_Pin == encodersPins.right) {
-		HAL_NVIC_DisableIRQ(EXTI3_IRQn);		
+		HAL_NVIC_DisableIRQ(EXTI3_IRQn);  //disable interrupt for the right encoder		
 		enc_counter.left++;
 		printf("counter %ld\n", enc_counter.left);
-		if (lock_right == 1) {
-			lock_right = 0;
+		if (lock_right == 1) {            
+			lock_right = 0;          //get the right encoder lock
 			for(int k = 0; k< 3000; k++){
 				k++;
-				k--;
+				k--;            //junk instructions to force the compiler not to optimize this loop
 				if(k == 2990){
-					encoders_distances.right++;
+					encoders_distances.right++;  //increase the distance for the right encoder and exit he loop
 					printf("distance = %ld\n", encoders_distances.right);
 					break;
 				}
 				else
 					continue;
 			}
-			HAL_NVIC_EnableIRQ(EXTI3_IRQn);
-			enc_counter.right = 0;
-			lock_right = 1;
+			HAL_NVIC_EnableIRQ(EXTI3_IRQn); //reenable right encoder interrupt
+			enc_counter.right = 0;       
+			lock_right = 1;               //release the lock
 		}
 		else
 			return;
 	}
 	else if (GPIO_Pin == encodersPins.left) {
-		HAL_NVIC_DisableIRQ(EXTI4_IRQn);		
+		HAL_NVIC_DisableIRQ(EXTI4_IRQn);	//disable interrupt for the left encoder	
 		enc_counter.left++;
 		printf("counter %ld\n", enc_counter.left);		
 		if (lock_left == 1) {
-			lock_left = 0;
+			lock_left = 0;        //get the left encoder lock
 
 			for(long int j = 0; j< 3000; j++){
 			j++;
-			j--;				
+			j--;	//junk instructions to force compiler not to optimize this loop
 				if(j == 2990){
-					encoders_distances.left++;
+					encoders_distances.left++;   //incerase left encoder distance and exit the loop
 					printf("distance = %ld\n", encoders_distances.left);
 					break;
 				}
 			}	
-			HAL_NVIC_EnableIRQ(EXTI4_IRQn);
-			lock_left = 1;
+			HAL_NVIC_EnableIRQ(EXTI4_IRQn);  //reenable left encoder interrupt
+			lock_left = 1;                   //release the lock
 		}
 		else{
 			printf("salamaki");
